afed5a84ff6919f1d59e4cdbe4fe5e8800cbe462
David Thong Nguyen <thnguyen2808@gmail.com>
2016-02-03 16:13:58 -0500

    New cake form is done (no database interaction yet)

--- app/add-cake-form.component.js
+++ app/add-cake-form.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', "./cake"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,26 +8,51 @@ System.register(['angular2/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, cake_1;
     var AddCakeFormComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (cake_1_1) {
+                cake_1 = cake_1_1;
             }],
         execute: function() {
             AddCakeFormComponent = (function () {
                 function AddCakeFormComponent() {
+                    this.model = new cake_1.Cake(10, "Easy Chocolate Chip Brownie Cheesecake", [
+                        "1 box of brownie mix, and ingredients needed to make them",
+                        "16 oz cream cheese, softened",
+                        "1 cup sugar",
+                        "1 tsp vanilla",
+                        "8 oz whipped cream or whipped topping",
+                        "1 cup chocolate chips"
+                    ], [
+                        "Prepare the brownie mix according to package instructions, and pour the batter into a springform pan.",
+                        "Bake according to package instructions, and cool completely.",
+                        "Meanwhile, beat cream cheese and sugar until smooth.",
+                        "Fold in vanilla and whipped cream, then fold in the chocolate chips.",
+                        "Spread over the cooled brownie in the springform pan."
+                    ]);
                     this.submitted = false;
+                    this.active = true;
                 }
-                AddCakeFormComponent.prototype.addCake = function (newCake, CakeList) {
-                    if (newCake) {
-                        CakeList.push(newCake);
-                    }
-                };
                 AddCakeFormComponent.prototype.onSubmit = function () {
                     this.submitted = true;
                 };
+                AddCakeFormComponent.prototype.addCake = function () {
+                    var _this = this;
+                    this.model = new cake_1.Cake(10, "", [], []);
+                    this.active = false;
+                    setTimeout(function () { return _this.active = true; }, 0);
+                };
+                Object.defineProperty(AddCakeFormComponent.prototype, "diagnostic", {
+                    // TODO: Remove this when we're done
+                    get: function () { return JSON.stringify(this.model); },
+                    enumerable: true,
+                    configurable: true
+                });
                 AddCakeFormComponent = __decorate([
                     core_1.Component({
                         selector: "add-cake-form",
--- app/add-cake-form.component.ts
+++ app/add-cake-form.component.ts
@@ -8,15 +8,35 @@ import {Cake}       from "./cake";
 })
 
 export class AddCakeFormComponent {
+
+    model = new Cake(10, "Easy Chocolate Chip Brownie Cheesecake", [
+        "1 box of brownie mix, and ingredients needed to make them",
+        "16 oz cream cheese, softened",
+        "1 cup sugar",
+        "1 tsp vanilla",
+        "8 oz whipped cream or whipped topping",
+        "1 cup chocolate chips"
+    ],
+    [
+        "Prepare the brownie mix according to package instructions, and pour the batter into a springform pan.",
+        "Bake according to package instructions, and cool completely.",
+        "Meanwhile, beat cream cheese and sugar until smooth.",
+        "Fold in vanilla and whipped cream, then fold in the chocolate chips.",
+        "Spread over the cooled brownie in the springform pan."
+    ]);
+
     submitted = false;
-
-    addCake(newCake:Cake, CakeList:Cake[]) {
-        if (newCake) {
-            CakeList.push(newCake);
-        }
-    }
-
     onSubmit() {
         this.submitted = true;
     }
+
+    active = true;
+    addCake() {
+        this.model = new Cake(10, "", [], []);
+        this.active = false;
+        setTimeout(() => this.active=true, 0);
+    }
+
+    // TODO: Remove this when we're done
+    get diagnostic() { return JSON.stringify(this.model); }
 }
