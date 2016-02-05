ef2745a13178e31b4dc93e712cf7685a0e7b6a01
David Thong Nguyen <thnguyen2808@gmail.com>
2016-02-05 15:55:01 -0500

    Designing colors

--- app/home.component.ts
+++ app/home.component.ts
@@ -3,20 +3,21 @@ import {Component}      from 'angular2/core';
 @Component({
     template: `
         <div class="home-container">
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
-            <h2>Welcome to Cake Book!</h2>
+            <h1 class="welcome-poster">Welcome to Cake Book!</h1>
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <!--<h2>Welcome to Cake Book!</h2>-->
+            <img src="https://trello-attachments.s3.amazonaws.com/5615b20e9c16c07464530290/1001x563/7532ac9b9bd6cd632980e2b7c610b941/921d4acd-26ab-43c8-b056-11a6abdb7c28.jpg">
         </div>
         `
 })
