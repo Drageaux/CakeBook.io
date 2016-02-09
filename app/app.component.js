4763218771c31157be7ce710bbcf902728abdae4
David Thong Nguyen <thnguyen2808@gmail.com>
2016-02-09 18:11:02 -0500

    Finished authentication; NOTE: still need route restriction

--- app/app.component.js
+++ app/app.component.js
@@ -46,7 +46,8 @@ System.register(["angular2/core", "angular2/http", 'angular2/router', "angular2-
             }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent() {
+                function AppComponent(authHttp) {
+                    this.authHttp = authHttp;
                     this.lock = new Auth0Lock('1w9uIYPLBxZzbciPImlhyG39EPDqzv8e', 'drageaux.auth0.com');
                 }
                 AppComponent.prototype.login = function () {
@@ -65,10 +66,15 @@ System.register(["angular2/core", "angular2/http", 'angular2/router', "angular2-
                 AppComponent.prototype.loggedIn = function () {
                     return angular2_jwt_1.tokenNotExpired();
                 };
+                /* Template for Getting Things */
+                AppComponent.prototype.getSecretThing = function () {
+                    this.authHttp.get('http://example.com/api/secretthing')
+                        .subscribe(function (data) { return console.log(data.json()); }, function (err) { return console.log(err); }, function () { return console.log('Complete'); });
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n        <nav class=\"navbar navbar-default navbar-fixed-top\">\n            <!-- Normal Menu -->\n            <ul class=\"nav navbar-nav navbar-right\" id=\"normalMenu\">\n                <li>\n                    <a class=\"navbar-item\" [routerLink]=\"['Home']\">\n                        <span class=\"glyphicon glyphicon-home\" aria-hidden=\"true\"></span>&nbsp;Home&nbsp;\n                    </a>\n                </li>\n                <li>\n                    <a class=\"navbar-item\" [routerLink]=\"['Cakes']\">\n                        <span class=\"glyphicon glyphicon-user\" aria-hidden=\"true\"></span>&nbsp;Profile&nbsp;\n                    </a>\n                </li>\n                <li>\n                    <a class=\"navbar-item\" href=\"#\">\n                        <span class=\"glyphicon glyphicon-cog\" aria-hidden=\"true\"></span>&nbsp;Settings&nbsp;\n                    </a>\n                </li>\n            </ul>\n        </nav>\n\n        <!--<h1>Welcome to Angular2 with Auth0</h1>-->\n        <!--<button *ngIf=\"!loggedIn()\" (click)=\"login()\">Login</button>-->\n        <!--<button *ngIf=\"loggedIn()\" (click)=\"logout()\">Logout</button>-->\n\n        <!--<router-outlet></router-outlet>-->\n\t\t",
+                        template: "\n\n        <div *ngIf=\"!loggedIn()\" class=\"login-container\">\n            <h1>Welcome to Angular2 with Auth0</h1>\n            <button *ngIf=\"!loggedIn()\" (click)=\"login()\">Login</button>\n            <button *ngIf=\"loggedIn()\" (click)=\"logout()\">Logout</button>\n        </div>\n\n        <div *ngIf=\"loggedIn()\">\n            <nav class=\"navbar navbar-default navbar-fixed-top\">\n                <!-- Normal Menu -->\n                <ul class=\"nav navbar-nav navbar-right\" id=\"normalMenu\">\n                    <li>\n                        <a class=\"navbar-item\" [routerLink]=\"['Home']\">\n                            <span class=\"glyphicon glyphicon-home\" aria-hidden=\"true\"></span>&nbsp;Home&nbsp;\n                        </a>\n                    </li>\n                    <li>\n                        <a class=\"navbar-item\" [routerLink]=\"['Cakes']\">\n                            <span class=\"glyphicon glyphicon-user\" aria-hidden=\"true\"></span>&nbsp;Profile&nbsp;\n                        </a>\n                    </li>\n                    <li>\n                        <a class=\"navbar-item\" href=\"#\">\n                            <span class=\"glyphicon glyphicon-cog\" aria-hidden=\"true\"></span>&nbsp;Settings&nbsp;\n                        </a>\n                    </li>\n                    <li>\n                        <a class=\"navbar-item\" (click)=\"logout()\">\n                            <span class=\"glyphicon glyphicon-log-out\" aria-hidden=\"true\"></span>&nbsp;Logout&nbsp;\n                        </a>\n                    </li>\n                </ul>\n            </nav>\n            <router-outlet></router-outlet>\n        </div>\n\t\t",
                         styleUrls: ["assets/stylesheets/style.css"],
                         encapsulation: core_3.ViewEncapsulation.None,
                         providers: [
@@ -76,7 +82,13 @@ System.register(["angular2/core", "angular2/http", 'angular2/router', "angular2-
                             cake_service_1.CakeService,
                             // in-memory web api providers
                             core_1.provide(http_2.XHRBackend, { useClass: core_2.InMemoryBackendService }),
-                            core_1.provide(core_2.SEED_DATA, { useClass: cake_data_1.CakeData }) // in-mem server data
+                            core_1.provide(core_2.SEED_DATA, { useClass: cake_data_1.CakeData }),
+                            core_1.provide(angular2_jwt_1.AuthHttp, {
+                                useFactory: function (http) {
+                                    return new angular2_jwt_1.AuthHttp(new angular2_jwt_1.AuthConfig(), http);
+                                },
+                                deps: [http_1.Http]
+                            })
                         ],
                         directives: [router_1.ROUTER_DIRECTIVES]
                     }),
@@ -86,7 +98,7 @@ System.register(["angular2/core", "angular2/http", 'angular2/router', "angular2-
                         { path: "/cakes", name: "Cakes", component: profile_component_1.ProfileComponent },
                         { path: "/cake/:id", name: "CakeDetail", component: cake_detail_component_1.CakeDetailComponent }
                     ]), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [angular2_jwt_1.AuthHttp])
                 ], AppComponent);
                 return AppComponent;
             })();
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, provide} from "angular2/core";
 import {HTTP_PROVIDERS, Http} from "angular2/http";
 import {RouteConfig, Router, APP_BASE_HREF, ROUTER_PROVIDERS, ROUTER_DIRECTIVES, CanActivate} from 'angular2/router';
 import {FORM_PROVIDERS} from 'angular2/common';
-import {AuthHttp, tokenNotExpired, JwtHelper} from "angular2-jwt";
+import {AuthHttp, tokenNotExpired, JwtHelper, AuthConfig} from "angular2-jwt";
 // in-memory web api imports
 import {XHRBackend} from "angular2/http";
 import {InMemoryBackendService, SEED_DATA} from 'a2-in-memory-web-api/core';
@@ -23,32 +23,41 @@ declare var Auth0Lock;
 @Component({
     selector: 'my-app',
     template: `
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <!-- Normal Menu -->
-            <ul class="nav navbar-nav navbar-right" id="normalMenu">
-                <li>
-                    <a class="navbar-item" [routerLink]="['Home']">
-                        <span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home&nbsp;
-                    </a>
-                </li>
-                <li>
-                    <a class="navbar-item" [routerLink]="['Cakes']">
-                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Profile&nbsp;
-                    </a>
-                </li>
-                <li>
-                    <a class="navbar-item" href="#">
-                        <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>&nbsp;Settings&nbsp;
-                    </a>
-                </li>
-            </ul>
-        </nav>
 
-        <!--<h1>Welcome to Angular2 with Auth0</h1>-->
-        <!--<button *ngIf="!loggedIn()" (click)="login()">Login</button>-->
-        <!--<button *ngIf="loggedIn()" (click)="logout()">Logout</button>-->
+        <div *ngIf="!loggedIn()" class="login-container">
+            <h1>Welcome to Angular2 with Auth0</h1>
+            <button *ngIf="!loggedIn()" (click)="login()">Login</button>
+            <button *ngIf="loggedIn()" (click)="logout()">Logout</button>
+        </div>
 
-        <!--<router-outlet></router-outlet>-->
+        <div *ngIf="loggedIn()">
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <!-- Normal Menu -->
+                <ul class="nav navbar-nav navbar-right" id="normalMenu">
+                    <li>
+                        <a class="navbar-item" [routerLink]="['Home']">
+                            <span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home&nbsp;
+                        </a>
+                    </li>
+                    <li>
+                        <a class="navbar-item" [routerLink]="['Cakes']">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Profile&nbsp;
+                        </a>
+                    </li>
+                    <li>
+                        <a class="navbar-item" href="#">
+                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>&nbsp;Settings&nbsp;
+                        </a>
+                    </li>
+                    <li>
+                        <a class="navbar-item" (click)="logout()">
+                            <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Logout&nbsp;
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <router-outlet></router-outlet>
+        </div>
 		`,
     styleUrls: ["assets/stylesheets/style.css"],
     encapsulation: ViewEncapsulation.None,
@@ -57,7 +66,13 @@ declare var Auth0Lock;
         CakeService,
         // in-memory web api providers
         provide(XHRBackend, {useClass: InMemoryBackendService}), // in-mem server
-        provide(SEED_DATA, {useClass: CakeData}) // in-mem server data
+        provide(SEED_DATA, {useClass: CakeData}), // in-mem server data
+        provide(AuthHttp, {
+            useFactory: (http) => {
+                return new AuthHttp(new AuthConfig(), http);
+            },
+            deps: [Http]
+        })
     ],
     directives: [ROUTER_DIRECTIVES]
 })
@@ -72,7 +87,7 @@ declare var Auth0Lock;
 export class AppComponent {
     lock = new Auth0Lock('1w9uIYPLBxZzbciPImlhyG39EPDqzv8e', 'drageaux.auth0.com');
 
-    constructor() {
+    constructor(public authHttp:AuthHttp) {
     }
 
     login() {
@@ -95,6 +110,17 @@ export class AppComponent {
 
     loggedIn() {
         return tokenNotExpired();
+    }
+
+
+    /* Template for Getting Things */
+    getSecretThing() {
+        this.authHttp.get('http://example.com/api/secretthing')
+            .subscribe(
+                data => console.log(data.json()),
+                err => console.log(err),
+                () => console.log('Complete')
+            );
     }
 }
 
