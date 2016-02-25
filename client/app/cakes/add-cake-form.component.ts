832b693d5e4c2a0a5a028e7337ce79f43526a5af
David Thong Nguyen <thnguyen2808@gmail.com>
2016-02-25 16:48:38 -0500

    Styled step list in add cake form

--- client/app/cakes/add-cake-form.component.ts
+++ client/app/cakes/add-cake-form.component.ts
@@ -25,7 +25,6 @@ export class AddCakeFormComponent {
     active = false;
 
     constructor(private _cakeService:CakeService) {
-        this.ingrList.push({"value": "combine 2 parts melted semi sweet chocolate with 3 parts ricotta cheese"})
     }
 
     openForm() {
