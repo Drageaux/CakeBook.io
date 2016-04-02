d0789033732f06a7029d502292f6340fa1c799f5
David Thong Nguyen <thnguyen2808@gmail.com>
2016-04-02 17:22:37 -0400

    Starting to validate strings client-side

--- client/app/cakes/editable-item-form.component.ts
+++ client/app/cakes/editable-item-form.component.ts
@@ -2,61 +2,7 @@ import {Component, OnInit, Input, Output, EventEmitter } from "angular2/core";
 
 @Component({
     selector: "editable-item-list",
-    template: `
-            <form class="form-group editable-item-list">
-                <label>{{listLabel}}</label><br>
-                <!-- Add Item -->
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="{{placeholder}}"
-                           [(ngModel)]="currItem">
-                    <span class="input-group-btn">
-                        <button type="submit" class="btn btn-primary"
-                                [class.disabled]="isEmptyString(currItem)"
-                                (click)="addItem(currItem)">
-                            Add
-                        </button>
-                    </span>
-                </div>
-                <!-- Item List -->
-                <ul *ngIf="itemList.length>0" class="list-group editable-item-list">
-                    <li *ngFor="#item of itemList; #idx = index" class="list-group-item">
-                        <div>
-                            <!-- Item number -->
-                            <div class="number-label"><span>{{idx+1}}</span></div>
-                            <!-- Item name -->
-                            <div class="list-group-item-name" [class.edit-item]="isEditing(idx)">
-                                <span [class.hidden]="isEditing(idx)">
-                                    {{item.value}}
-                                </span>
-                                <input #editValue type="text" class="form-control"
-                                       [class.hidden]="!isEditing(idx)"
-                                       [value]="item.value" autofocus>
-                            </div>
-                            <!-- Item buttons -->
-                            <div class="edit-remove" [class.hidden]="isEditing(idx)">
-                                <button type="button" class="btn edit-button"
-                                        [class.hidden]="isEditing(idx)"
-                                        (click)="editItem(idx)">
-                                    <span class="glyphicon glyphicon-pencil"></span>
-                                </button>
-                                <button type="button" class="btn remove-button"
-                                        [class.hidden]="isEditing(idx)"
-                                        (click)="removeItem(idx)">
-                                    <span class="glyphicon glyphicon-minus"></span>
-                                </button>
-                            </div>
-                            <div class="save-cancel" [class.hidden]="!isEditing(idx)">
-                                <button type="submit" class="btn btn-success save-button"
-                                        (click)="saveEdit(idx, editValue.value)">
-                                    <span class="glyphicon glyphicon-check"></span></button>
-                                <button type="button" class="btn btn-danger cancel-button"
-                                        (click)="cancelEdit(idx)">
-                                    <span class="glyphicon glyphicon-remove"></span></button>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </form>`
+    templateUrl: "templates/editable-item-form.component.html"
 })
 
 export class EditableItemForm implements OnInit {
@@ -110,5 +56,12 @@ export class EditableItemForm implements OnInit {
 
     isEmptyString(str:string) {
         return str == "" || str == null;
+    }
+
+    isValidInput(str:string) {
+        if (!this.isEmptyString(str)) {
+            return str.length > 10
+        }
+        return false
     }
 }
