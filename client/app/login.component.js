35013e3a159cd00924ac88f39ca7a8f4bb635ad7
David Thong Nguyen <thnguyen2808@gmail.com>
2016-04-27 23:38:32 -0400

    Fixing remaining imports error for new version

--- client/app/login.component.js
+++ client/app/login.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/router'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', "angular2/platform/common"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +8,7 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1;
+    var core_1, router_1, common_1;
     var LoginComponent;
     return {
         setters:[
@@ -17,6 +17,9 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1) {
             },
             function (router_1_1) {
                 router_1 = router_1_1;
+            },
+            function (common_1_1) {
+                common_1 = common_1_1;
             }],
         execute: function() {
             LoginComponent = (function () {
@@ -50,7 +53,7 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1) {
                     core_1.Component({
                         templateUrl: "templates/login.component.html"
                     }), 
-                    __metadata('design:paramtypes', [router_1.Router, router_1.Location])
+                    __metadata('design:paramtypes', [router_1.Router, common_1.Location])
                 ], LoginComponent);
                 return LoginComponent;
             })();
